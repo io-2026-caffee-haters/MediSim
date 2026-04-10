7c34f352e26f01858adda9d320e38233d590940b
Maria Bagińska <m.baginska36@gmail.com>
2026-04-10 05:31:55 +0200

    New test added

--- src/Assets/Scripts/Managers/PatientManager.cs
+++ src/Assets/Scripts/Managers/PatientManager.cs
@@ -1,6 +1,9 @@
 public class PatientManager
 {
-    // W prawdziwej grze będą ukryte za prywatnym setem, ale na razie potrzebujemy ich do testów
     public Patient CurrentPatient { get; set; } 
     public MedicalRecord CurrentRecord { get; set; }
+    public void SpawnNewPatient()
+    {
+        
+    }
 }

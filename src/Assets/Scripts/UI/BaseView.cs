81e742ca9d8a26651ff83bbb7e885c9d55a4fb3c
Maria Bagińska <m.baginska36@gmail.com>
2026-04-17 14:22:47 +0200

    uimanager + baseview

---
+++ src/Assets/Scripts/UI/BaseView.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public abstract class BaseView : MonoBehaviour{
+    public bool isPopup = false;
+    protected UIManager _uiManager;
+    public virtual void Initialize(UIManager manager){ _uiManager = manager; }
+
+    //ulatwienie co do zamykania/otwierania
+    public virtual void Show(){ gameObject.SetActive(true); }
+    public virtual void Hide(){ gameObject.SetActive(false); }
+}

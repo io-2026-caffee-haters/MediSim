0a3b171983363ee68129e807406ab3e9590fbefd
Jan Holzer <jan.m.holzer@gmail.com>
2026-05-12 19:02:13 +0200

    Implementacja zapisu i odczytu gry, closes #15

--- src/Assets/Scripts/Core/SaveSystem.cs
+++ src/Assets/Scripts/Core/SaveSystem.cs
@@ -1,10 +1,31 @@
 using UnityEngine;
+using System.IO;
 
 public class SaveSystem : MonoBehaviour
 {
+    // Pomocnicza metoda, żeby nie kopiować ścieżki
+    private string GetPath()
+    {
+        return Path.Combine(Application.persistentDataPath, "savegame.json");
+    }
 
     public void SaveCurrentGame(SaveData data)
     {
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(GetPath(), json);
+        Debug.Log("Zapisano grę w: " + GetPath());
+    }
 
+    public SaveData LoadGame()
+    {
+        if (File.Exists(GetPath()))
+        {
+            string json = File.ReadAllText(GetPath());
+            Debug.Log("Wczytano grę z: " + GetPath());
+            return JsonUtility.FromJson<SaveData>(json);
+        }
+        
+        Debug.LogWarning("Brak pliku zapisu!");
+        return null;
     }
 }
